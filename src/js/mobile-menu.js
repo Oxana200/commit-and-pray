9bf6e5b9ad0a8c8884dd078174119443db977b91
oxana <oxanamoisiuk7@gmail.com>
2024-11-30 18:55:58 +0200

    all

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,12 +3,21 @@
     openModalBtn: document.querySelector('[data-menu-open]'),
     closeModalBtn: document.querySelector('[data-menu-close]'),
     modal: document.querySelector('[data-menu]'),
+    menuLinks: document.querySelectorAll('[data-menu] a'), // Додаємо посилання всередині меню
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
+  // Функція для відкриття/закриття меню
   function toggleModal() {
     refs.modal.classList.toggle('is-open');
   }
+
+  // Додаємо обробник для кожного посилання в меню
+  refs.menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+      refs.modal.classList.remove('is-open'); // Закриваємо меню
+    });
+  });
 })();
